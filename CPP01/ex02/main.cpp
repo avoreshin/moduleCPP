291a573b665a2e2fac93f0cae8c2a067f41bbfcf
Andrey Oreshin <avoreshin@gmail.com>
2022-04-18 20:55:54 +0300

    feat: CPP01 done!

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -17,7 +17,6 @@ int main (void){
 	std::string *stringPTR = &string;
 	std::string &stringREF = string;
 
-
 	std::cout <<"The memory address of the string variable: "
 	<< &string<< std::endl;
 	std::cout <<"The memory address held by stringPTR: "
@@ -25,10 +24,10 @@ int main (void){
 	std::cout <<"The memory address held by stringREF: "
 	<< &stringREF << std::endl;
 	std::cout <<"The value of the string variable: "
-			 << string << std::endl;
+	<< string << std::endl;
 	std::cout <<"The value pointed to by stringPTR: "
-			<< *stringPTR  << std::endl;
+	<< *stringPTR  << std::endl;
 	std::cout <<"The value pointed to by stringREF: "
-			  << stringREF  << std::endl;
+	<< stringREF << std::endl;
 	return 0;
 }
---
+++ CPP01/ex06/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlamonic <jlamonic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 23:02:51 by jlamonic          #+#    #+#             */
+/*   Updated: 2022/04/17 23:08:43 by jlamonic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_H
+# define HARL_H
+
+#include <ostream>
+
+class Harl{
+private:
+	void debug(void);
+	void info (void);
+	void warning (void);
+	void error (void);
+
+public:
+	void complain(std::string level);
+	Harl();
+	~Harl();
+	
+};
+
+#endif
