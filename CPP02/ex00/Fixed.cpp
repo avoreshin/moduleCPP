5e46dedbe097c59b54f728050d9ebc6dbec8c28a
avoreshin <avoreshin@gmail.com>
2022-04-20 15:12:00 +0300

    feat: add class

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -1,5 +1,55 @@
-//
-// Created by Jesica lamonictestbs on 4/19/22.
-//
-
 #include "Fixed.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Fixed::Fixed()
+{
+}
+
+Fixed::Fixed( const Fixed & src )
+{
+}
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Fixed::~Fixed()
+{
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Fixed &				Fixed::operator=( Fixed const & rhs )
+{
+	//if ( this != &rhs )
+	//{
+		//this->_value = rhs.getValue();
+	//}
+	return *this;
+}
+
+std::ostream &			operator<<( std::ostream & o, Fixed const & i )
+{
+	//o << "Value = " << i.getValue();
+	return o;
+}
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+
+/* ************************************************************************** */
--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -1,13 +1,24 @@
 #ifndef FIXED_HPP
-#define FIXED_HPP
+# define FIXED_HPP
 
-#include <iostream>
+# include <iostream>
+# include <string>
 
-class Fixed {
-public:
-    Fixed();
-    ~Fixed();
+class Fixed
+{
+
+	public:
+
+		Fixed();
+		Fixed( Fixed const & src );
+		~Fixed();
+
+		Fixed &		operator=( Fixed const & rhs );
+
+	private:
+
 };
 
+std::ostream &			operator<<( std::ostream & o, Fixed const & i );
 
-#endif
+#endif /* *********************************************************** FIXED_H */
