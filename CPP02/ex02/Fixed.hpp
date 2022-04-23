12ffd5191f0c228d4f069b2f739e943a058f7bce
Andrey Oreshin <avoreshin@gmail.com>
2022-04-23 23:20:09 +0300

    feat: add class Fixed

---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlamonic <jlamonic@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 23:07:42 by jlamonic          #+#    #+#             */
+/*   Updated: 2022/04/23 23:07:48 by jlamonic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+
+class Fixed {
+
+public:
+	Fixed(void);
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed(const Fixed & fixed);
+	Fixed &operator = (const Fixed &fixed);
+	~Fixed(void);
+
+	bool operator==(const Fixed &fixed) const;
+	bool operator>(const Fixed &fixed) const;
+	bool operator<(const Fixed &fixed) const;
+	bool operator>=(const Fixed &fixed) const;
+	bool operator<=(const Fixed &fixed) const;
+	bool operator!=(const Fixed &fixed) const;
+
+	Fixed operator+(const Fixed &fixed) const;
+	Fixed operator-(const Fixed &fixed) const;
+	Fixed operator*(const Fixed &fixed) const;
+	Fixed operator/(const Fixed &fixed) const;
+
+	Fixed &operator++(void);
+	Fixed &operator--(void);
+	Fixed operator++(void);
+	Fixed operator--(void);
+
+	static const Fixed &max(Fixed &f1, Fixed &f2);
+	static const Fixed &max(const Fixed &f1, const Fixed &f2);
+	static const Fixed &min(Fixed &f1, Fixed &f2);
+	static const Fixed &min(const Fixed &f1, const Fixed &f2);
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+	float toFloat(void) const;
+	int toInt(void) const;
+
+private:
+	static const int _fractionalBits;
+	int _value;
+};
+
+std::ostream &operator<<(std::ostream &os, const Fixed &fixed);
+
+#endif
