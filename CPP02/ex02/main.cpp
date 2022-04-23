624be79f6a486b7381563edb312016fe05b8e80f
Andrey Oreshin <avoreshin@gmail.com>
2022-04-23 23:20:34 +0300

    feat: add main, Makefile

---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlamonic <jlamonic@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/23 23:07:58 by jlamonic          #+#    #+#             */
+/*   Updated: 2022/04/23 23:12:39 by jlamonic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Fixed.hpp"
+
+int main( void ) {
+  Fixed a;
+  Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+  std::cout << a << std::endl;
+  std::cout << ++a << std::endl;
+  std::cout << a << std::endl;
+  std::cout << a++ << std::endl;
+  std::cout << a << std::endl;
+  std::cout << b << std::endl;
+
+  std::cout << Fixed::max( a, b ) << std::endl;
+
+  return 0;
+}
