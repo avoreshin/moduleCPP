7b6baaea27188f38916518e725b538f6739664e6
Andrey Oreshin <avoreshin@gmail.com>
2022-04-25 21:20:39 +0300

    feat: class Point

--- CPP02/ex03/Point.cpp
+++ CPP02/ex03/Point.cpp
@@ -1,5 +1,59 @@
-//
-// Created by Jesica Lamonica on 4/25/22.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlamonic <jlamonic@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 18:01:47 by jlamonic          #+#    #+#             */
+/*   Updated: 2022/04/25 18:01:49 by jlamonic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "Point.hpp"
+
+Point::Point(void) : _x(0), _y(0){
+}
+
+Point::Point(const Point &point) {
+	*this = point;
+}
+
+Point::Point(Fixed const &x,  Fixed const &y) : _x(x),_y(y) {};
+
+Point &Point::operator=(Point const &point) {
+	(Fixed) this->_x = (Fixed)point._x;
+	(Fixed) this->_y = (Fixed)point._y;
+	return (*this);
+}
+
+Point Point::operator-(Point const &point) const {
+	Point p;
+	p.setX(this->_x - point._x);
+	p.setY(this->_y - point._y);
+	return p;
+}
+
+Fixed const &Point::getX() const {
+	return this->_x;
+}
+
+Fixed const &Point::getY() const {
+	return this->_y;
+}
+
+void Point::setX(const Fixed &fixed) {
+	const_cast <Fixed &>(this->_x) = fixed.getRawBits();
+}
+
+void Point::setY(const Fixed &fixed) {
+	const_cast <Fixed &>(this->_y) = fixed.getRawBits();
+}
+
+bool Point::operator==(const Point &point) {
+	if ((Fixed)this->_x == point._x && (Fixed)this->_y == point._y)
+		return true;
+	return false;
+}
+
+Point::~Point(void) {};
--- CPP02/ex03/Point.hpp
+++ CPP02/ex03/Point.hpp
@@ -1,14 +1,42 @@
-//
-// Created by Jesica Lamonica on 4/25/22.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlamonic <jlamonic@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 18:01:51 by jlamonic          #+#    #+#             */
+/*   Updated: 2022/04/25 18:01:58 by jlamonic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef MODULECPP_POINT_HPP
-#define MODULECPP_POINT_HPP
+#ifndef POINT_HPP
+#define POINT_HPP
 
+#include "iostream"
+#include "Fixed.hpp"
 
 class Point {
+public:
+	Point(void);
+	Point(Fixed const &x, Fixed const &y);
+	Point(Point const &point);
+	~Point(void);
 
+	Point &operator=(Point const &point);
+	bool operator==(Point const &point);
+
+	Point operator-(const Point &point) const;
+
+	Fixed const &getX(void) const;
+	Fixed const &getY(void) const;
+	void setX(const Fixed &fixed);
+	void setY(const Fixed &fixed);
+
+private:
+	Fixed const _x;
+	Fixed const _y;
 };
 
 
-#endif //MODULECPP_POINT_HPP
+#endif
