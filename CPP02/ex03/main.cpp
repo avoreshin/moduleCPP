6b9b3855c073307903853b7cdc0ecb8536a808b3
Andrey Oreshin <avoreshin@gmail.com>
2022-04-25 21:20:51 +0300

    feat: add main

--- CPP02/ex03/main.cpp
+++ CPP02/ex03/main.cpp
@@ -1,4 +1,41 @@
-//
-// Created by Jesica Lamonica on 4/25/22.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlamonic <jlamonic@student.21-school.ru    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 18:01:38 by jlamonic          #+#    #+#             */
+/*   Updated: 2022/04/25 18:01:42 by jlamonic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+#include "Fixed.hpp"
+#include "Point.hpp"
+#include "Bsp.hpp"
+
+int main (void)
+{
+	std::cout.setf(std::ios::boolalpha);
+	std::cout << bsp::bsp(Point(0.0f, 0.0f), Point(0.0f, 10.0f), Point(10.0f, 0.0f),
+						  Point(2.0f, 2.0f))
+			  << std::endl; // false
+	std::cout << bsp::bsp(Point(0.0f, 0.0f), Point(0.0f, 1.0f), Point(1.0f, 0.0f),
+						  Point(0.0f, 0.0f))
+			  << std::endl; // false
+	std::cout << bsp::bsp(Point(0.0f, 0.0f), Point(0.0f, 1.0f), Point(1.0f, 0.0f),
+						  Point(0.4f, 0.4f))
+			  << std::endl; // true
+	std::cout << bsp::bsp(Point(0.0f, 0.0f), Point(0.0f, 2.0f), Point(2.0f, 0.0f),
+						  Point(0.5f, 0.5f))
+			  << std::endl; // true
+	std::cout << bsp::bsp(Point(0.0f, 0.0f), Point(0.0f, 2.0f), Point(2.0f, 0.0f),
+						  Point(1.0f, 1.0f))
+			  << std::endl; // false
+	return 0;
+
+}
+
 
