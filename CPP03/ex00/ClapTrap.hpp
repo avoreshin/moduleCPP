5039ab3d3482a2069bab73bb38a602d66a2ce87e
avoreshin <avoreshin@gmail.com>
2022-04-27 21:30:41 +0300

    init: ex00

---
+++ CPP03/ex00/ClapTrap.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlamonic <jlamonic@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 21:26:11 by jlamonic          #+#    #+#             */
+/*   Updated: 2022/04/27 21:26:11 by jlamonic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MODULECPP_CLAPTRAP_HPP
+#define MODULECPP_CLAPTRAP_HPP
+
+
+class ClapTrap {
+
+};
+
+
+#endif //MODULECPP_CLAPTRAP_HPP
